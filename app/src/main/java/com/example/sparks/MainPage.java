7299ec47f97d44d9c2b880655b06a11712ef5a16
Slayde <crce.9638.ce@gmail.com>
2022-10-04 22:57:32 +0530

    version 1

---
+++ app/src/main/java/com/example/sparks/MainPage.java
@@ -0,0 +1,13 @@
+package com.example.sparks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
--- app/src/main/java/com/example/sparks/operator.java
+++ app/src/main/java/com/example/sparks/operator.java
@@ -2,7 +2,12 @@ package com.example.sparks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class operator extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class operator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operator);
+        EditText username =(EditText) findViewById(R.id.username);
+        EditText password =(EditText) findViewById(R.id.password);
+        Button loginbtn=(Button) findViewById(R.id.btn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+                {
+                    Toast.makeText(operator.this,"Login Sucessful",Toast.LENGTH_SHORT).show();
+                    MainPage();
+                }
+                else
+                {
+                    Toast.makeText(operator.this,"Login Unsucessful",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    public void MainPage(){
+        Intent intent =new Intent(this,MainPage.class);
+        startActivity(intent);
     }
 }
