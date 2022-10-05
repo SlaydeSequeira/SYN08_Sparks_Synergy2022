33526da256af1d4f9469907104a9f9e23fd0b77c
Slayde <crce.9638.ce@gmail.com>
2022-10-05 05:55:27 +0530

    Version 5

--- app/src/main/java/com/example/sparks/payment.java
+++ app/src/main/java/com/example/sparks/payment.java
@@ -3,6 +3,9 @@ package com.example.sparks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class payment extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class payment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        Button btn =(Button) findViewById(R.id.btn);
+        EditText t =(EditText) findViewById(R.id.text1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PaymentGatewayStart();
+            }
+        });
+    }
+    public void PaymentGatewayStart(){
+
     }
 }
